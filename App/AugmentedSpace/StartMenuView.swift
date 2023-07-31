4a99eab9a37b3bbddcef693fd8c1110a0f05dabe
tristanhaeuser <tristanhaeuser@icloud.com>
2023-07-31 19:14:49 +0200

    Updated StartMenuView

--- App/AugmentedSpace/StartMenuView.swift
+++ App/AugmentedSpace/StartMenuView.swift
@@ -9,17 +9,34 @@ import SwiftUI
 
 struct StartMenuView: View {
     var body: some View {
-
-        Text("Welcome to")
-        Text("AR-Stronaut")
-            .font(.system(size: 48))
-              .padding(50)
-              .background(
-                Image("spaceBackground")
-                      .resizable()
-                      .scaledToFit()
-                      .frame(maxWidth: .infinity, maxHeight: .infinity)
-              )
+        ZStack(alignment: .leading) {
+            Image("spaceBackground")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+//            HStack {
+               
+                VStack(alignment: .leading) {
+                   Spacer(minLength: 50)
+                    Text("Welcome to")
+                        .foregroundColor(Color.white)
+                    HStack {
+                        Text("AR-")
+                            .foregroundColor(Color(red: 0.953, green: 0.262, blue: 0.129))
+                        Text("Stronaut")
+                            .foregroundColor(Color.white)
+                    }
+                    .font(.system(size: 48))
+                    Text("This is a two player game.")
+                        .foregroundColor(Color(red: 0.953, green: 0.262, blue: 0.129))
+                    Text ("Find a teammate and select your side")
+                        .foregroundColor(Color.white)
+                    Spacer(minLength: 700)
+                }
+                .padding(EdgeInsets(top: 0, leading: 50, bottom: 0, trailing: 0))
+                
+            }
+//        }
     }
 }
 
