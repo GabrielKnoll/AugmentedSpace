c61add80b0e8ede87d4f67bf83d17cc0c6abbb75
tristanhaeuser <tristanhaeuser@icloud.com>
2023-08-01 10:32:35 +0200

    Integrated Poppins Font

--- App/AugmentedSpace/StartMenuView.swift
+++ App/AugmentedSpace/StartMenuView.swift
@@ -14,29 +14,34 @@ struct StartMenuView: View {
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
-//            HStack {
-
+            VStack(alignment: .leading) {
                 VStack(alignment: .leading) {
-                   Spacer(minLength: 50)
+                    Spacer()
+                        .frame(maxHeight: 75)
                     Text("Welcome to")
+                        .font(Font.custom("Poppins-SemiBold", size: 18))
                         .foregroundColor(Color.white)
                     HStack {
                         Text("AR-")
+                            .font(Font.custom("Poppins-SemiBold", size: 47))
                             .foregroundColor(Color(red: 0.953, green: 0.262, blue: 0.129))
                         Text("Stronaut")
                             .foregroundColor(Color.white)
                     }
                     .font(.system(size: 48))
+                    Spacer()
                     Text("This is a two player game.")
                         .foregroundColor(Color(red: 0.953, green: 0.262, blue: 0.129))
                     Text("Find a teammate and select your side")
                         .foregroundColor(Color.white)
-                    Spacer(minLength: 700)
                 }
                 .padding(EdgeInsets(top: 0, leading: 50, bottom: 0, trailing: 0))
-
+                Spacer(minLength: 100)
+                StartGameButton(text: "Start Game") {
+                }
+                .padding(5.0)
+            }
         }
-//        }
     }
 }
 
